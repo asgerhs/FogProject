f6b5bfda52c1d60c38c5b378e508e2332976c3ee
WSHuusfeldt <williamhuusfeldt@gmail.com>
2019-04-24 11:43:52 +0200

    Created class MaterialList

---
+++ FogProject/src/main/java/data/MaterialList.java
@@ -0,0 +1,15 @@
+package data;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author William Sehested Huusfeldt
+ */
+public class MaterialList {
+    
+    private ArrayList<material> stockList;
+
+   
+
+}
