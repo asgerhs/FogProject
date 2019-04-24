bfa7a3e8ee3c605483de66a3407011a9728012d2
Krigermus <zyjinxx@hotmail.com>
2019-04-24 12:01:39 +0200

    Update MaterialList.java

--- FogProject/src/main/java/data/MaterialList.java
+++ FogProject/src/main/java/data/MaterialList.java
@@ -11,15 +11,15 @@ public class MaterialList {
     private ArrayList<material> stockList;
 
     public MaterialList() {
-        stockList = new ArrayList<material>();
+        stockList = new ArrayList();
     }
 
     public ArrayList<material> getStockList() {
         return stockList;
     }
 
-    public void addMaterialToStockList(material arg) { //arg værende en buffer for fremtidigt navn
-        stockList.add(arg);
+    public void addMaterialToStockList(material mat) {
+        stockList.add(mat);
     }
 
 }
