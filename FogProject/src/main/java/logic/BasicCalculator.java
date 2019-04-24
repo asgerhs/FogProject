70bae270c351cd50a9290bdf87305e8f132bc376
Krigermus <zyjinxx@hotmail.com>
2019-04-24 11:42:35 +0200

    Update BasicCalculator.java

--- FogProject/src/main/java/logic/BasicCalculator.java
+++ FogProject/src/main/java/logic/BasicCalculator.java
@@ -5,26 +5,77 @@ package logic;
  * @author Martin Frederiksen
  */
 public class BasicCalculator {
-    private double length;
-    private double width;
+    private int length;
+    private int width;
 
     public BasicCalculator() {
-        this.length = 7.80;
-        this.width = 6.00;
+        this.length = 780;
+        this.width = 600;
     }
     
     //Calculating the square of carport
     public int calcPosts(){
-        return 4 + (int) (length / 5) * 2 + 1;
+        return 4 + ((length / 500) * 2) + 1;
     }
-    
+        
+    public int calcRem(){
+        // length > 600 use if statement when we use arbitrary value
+        return (length % 480 == 0) ? (length / 480) * 2 : ((length / 480) + 1) * 2;
+    }
     
     //Calculating the roof of carport
     public int calcRafters(){
-        return (int) (length / .5);
+        return length / 50;
     }
     
+    public int calcTopFasciasFront(){
+        return (width % 360 == 0) ? width / 360 : (width / 360) + 1;
+    }
     
+    public int calcTopFasciasSide(){
+        return (width % 540 == 0) ? (width / 540) * 2 : ((width / 540) + 1) * 2;
+    }
+    
+    public int calcBottomFasciasFB(){
+        return (width % 360 == 0) ? (width / 360) * 2 : ((width / 360) + 1) * 2;
+    } 
+    
+    public int calcBottomFasciasSide(){
+        return (length % 540 == 0) ? (length / 540) * 2 : ((length / 540) + 1) * 2;
+    } 
+    
+    public int calcWaterBoardFront(){
+        return (width % 360 == 0) ? width / 360 : (width / 360) + 1;
+    }
+    
+    public int calcWaterBoardSide(){
+        return (length % 540 == 0) ? (length / 540) * 2 : ((length / 540) + 1) * 2;
+    }
+    
+    //RoofingSheets need rework to specify
+    /*
+    int calcLength = length;
+        int rest = 0;
+        if(calcLength >= 600) {
+            rest = calcLength - 600;
+            calcLength = 600;
+        }
+        
+        switch(length) {
+            case (length > 0): return 0;
+            
+            
+            default: return 0;
+        }
+    */
+    //Big quant = small quant?? maybe calc length of different sheets
+    public int calcRoofingSheetsBig(){
+        return (width % 100 == 0) ? width / 100 : width / 100 + 1;
+    }
+    
+    public int calcRoofingSheetsSmall(){
+        return (width % 100 == 0) ? width / 100 : width / 100 + 1;
+    }
     //Calculating the pieces of carport
     public int calcBracketsRight(){
         return calcRafters();
@@ -36,15 +87,15 @@ public class BasicCalculator {
     
     public int calcScrewPackages(){
         int screws = 0;
-        screws += calcBracketsRight()*6;
-        screws += calcBracketsLeft()*6;
+        screws += calcBracketsRight()*9;
+        screws += calcBracketsLeft()*9;
         
         return (screws % 250 == 0) ? screws / 250 : (screws / 250) + 1;
     }
     
     public static void main(String[] args) {
         BasicCalculator bc = new BasicCalculator();
-        System.out.println(bc.calcScrewPackages());
+        System.out.println(bc.calcRoofingSheetsSmall());
         
     }
 }
