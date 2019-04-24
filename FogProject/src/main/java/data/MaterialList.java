dbf49cc2d5779ad517ebbabb91c49f910fdaa78e
WSHuusfeldt <williamhuusfeldt@gmail.com>
2019-04-24 11:57:16 +0200

    Added constructor, getMethod and addMethod

--- FogProject/src/main/java/data/MaterialList.java
+++ FogProject/src/main/java/data/MaterialList.java
@@ -7,9 +7,19 @@ import java.util.ArrayList;
  * @author William Sehested Huusfeldt
  */
 public class MaterialList {
-    
+
     private ArrayList<material> stockList;
 
-   
+    public MaterialList() {
+        stockList = new ArrayList<material>();
+    }
+
+    public ArrayList<material> getStockList() {
+        return stockList;
+    }
+
+    public void addMaterialToStockList(material arg) { //arg værende en buffer for fremtidigt navn
+        stockList.add(arg);
+    }
 
 }
