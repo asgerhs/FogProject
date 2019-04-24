5b0a3fe0c8b54671d94e2e95a8bf708af95e4a8c
WSHuusfeldt <williamhuusfeldt@gmail.com>
2019-04-24 10:08:01 +0200

    Added tables Stock and Users to script

---
+++ Database/FogProject_Script.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `fogproject`.`stock` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `length` INT NULL,
+  `qty` INT NOT NULL,
+  `unit` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(150) NOT NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `fogproject`.`users` (
+  `username` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `role` ENUM('ADMIN', 'SALESMAN') NOT NULL,
+  PRIMARY KEY (`username`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
+
--- FogProject/src/main/java/data/material.java
+++ FogProject/src/main/java/data/material.java
@@ -33,4 +33,6 @@ public class material {
         return description;
     }
     
+    
+    
 }
