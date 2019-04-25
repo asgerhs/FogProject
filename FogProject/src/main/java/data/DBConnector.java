931c8feee328b9d72a6222fd1996ee2daf92b57b
Andreas Vikke <vikkedesign@gmail.com>
2019-04-25 13:17:35 +0200

    Created Try with in DataMapper

--- FogProject/src/main/java/data/DBConnector.java
+++ FogProject/src/main/java/data/DBConnector.java
@@ -2,21 +2,14 @@ package data;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Properties;
 
 /**
  *
  * @author Asger Hermind Sørensen
  */
 public class DBConnector {
-
-    private ResultSet rs;
-    
-    
-    
     public static final String DRIVER = "com.mysql.jdbc.Driver";
     public static final String URL = "jdbc:mysql://157.230.110.215:3306/fogproject";
     public static final String USER = "root";
--- FogProject/src/main/java/data/MaterialMapper.java
+++ FogProject/src/main/java/data/MaterialMapper.java
@@ -4,8 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -22,22 +22,21 @@ public class MaterialMapper {
      * @throws SQLException 
      */
     public List<Material> getMaterials() throws SQLException {
-        List<Material> materials = new ArrayList();
-            Connection con = connector.getConnection();
+        try (Connection con = connector.getConnection()) {
+            List<Material> materials = new ArrayList();
             String qry = "SELECT * FROM stock";
-            PreparedStatement ps = con.prepareStatement(qry);
-            ResultSet rs = ps.executeQuery();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(qry);
 
             while (rs.next()) {
                 materials.add(new Material(rs.getString("name"),
-                        rs.getInt("length"),
-                        rs.getInt("qty"),
-                        rs.getString("unit"),
-                        rs.getString("description")));
+                rs.getInt("length"),
+                rs.getInt("qty"),
+                rs.getString("unit"),
+                rs.getString("description")));
             }
-            con.close();
-        return materials;
-
+            return materials;
+        }
     }
 
     /**
@@ -46,27 +45,24 @@ public class MaterialMapper {
      * @return material matching id in param
      * @throws SQLException 
      */
-    public List<Material> getMaterialByID(int id) throws SQLException {
-        
-        Connection con = connector.getConnection();
-        List<Material> materials = new ArrayList();
-        String qry = "SELECT * FROM stock WHERE id = ?";
-        PreparedStatement ps = con.prepareStatement(qry);
+    public List<Material> getMaterialById(int id) throws SQLException {
+        try (Connection con = connector.getConnection()) {
+            List<Material> materials = new ArrayList();
+            String qry = "SELECT * FROM stock WHERE id = ?";
+            
+            PreparedStatement ps = con.prepareStatement(qry);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
 
-        ps.setInt(1, id);
-
-        ResultSet rs = ps.executeQuery();
-
-        while (rs.next()) {
-            materials.add(new Material(rs.getString("name"),
-                    rs.getInt("length"),
-                    rs.getInt("qty"),
-                    rs.getString("unit"),
-                    rs.getString("description")));
+            while (rs.next()) {
+                materials.add(new Material(rs.getString("name"),
+                rs.getInt("length"),
+                rs.getInt("qty"),
+                rs.getString("unit"),
+                rs.getString("description")));
+            }
+            return materials;
         }
-        con.close();
-        return materials;
-        
     }
 
     public static void main(String[] args) throws SQLException {
