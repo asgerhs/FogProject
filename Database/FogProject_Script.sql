11463cbfb37536d51fc1ed43757c7e8562f2a408
Andreas Vikke <vikkedesign@gmail.com>
2019-04-30 11:50:53 +0200

    New SQL Script

--- Database/FogProject_Script.sql
+++ Database/FogProject_Script.sql
@@ -1,5 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS `fogproject`;
 DROP TABLE IF EXISTS `fogproject`.`users`;
+DROP TABLE IF EXISTS `fogproject`.`stockToCategory`;
+DROP TABLE IF EXISTS `fogproject`.`categories`;
 DROP TABLE IF EXISTS `fogproject`.`stock`;
 
 CREATE TABLE `fogproject`.`stock` (
@@ -8,7 +10,31 @@ CREATE TABLE `fogproject`.`stock` (
   `length` INT NOT NULL,
   `amount` INT NOT NULL,
   `unit` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`ref`, `length`));
+  PRIMARY KEY (`ref`)
+);
+
+CREATE TABLE `fogproject`.`categories` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `fogproject`.`stockToCategory` (
+  `stockRef` VARCHAR(100) NOT NULL,
+  `categoryId` INT NOT NULL,
+  PRIMARY KEY (`stockRef`, `categoryId`),
+  INDEX `categoryId_FK_idx` (`categoryId` ASC) VISIBLE,
+  CONSTRAINT `stockRef_FK`
+    FOREIGN KEY (`stockRef`)
+    REFERENCES `fogproject`.`stock` (`ref`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `categoryId_FK`
+    FOREIGN KEY (`categoryId`)
+    REFERENCES `fogproject`.`categories` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
 
 CREATE TABLE `fogproject`.`users` (
   `username` VARCHAR(45) NOT NULL,
@@ -16,5 +42,6 @@ CREATE TABLE `fogproject`.`users` (
   `password` VARCHAR(100) NOT NULL,
   `role` ENUM('ADMIN', 'SALESMAN') NOT NULL,
   PRIMARY KEY (`username`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE
+);
 
