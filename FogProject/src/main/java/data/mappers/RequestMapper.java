4fc5a6a6715677c6714197eb90ddd65f429c1c58
WSHuusfeldt <williamhuusfeldt@gmail.com>
2019-05-03 10:44:37 +0200

    added getById() in RequestMapper

--- FogProject/src/main/java/data/mappers/RequestMapper.java
+++ FogProject/src/main/java/data/mappers/RequestMapper.java
@@ -5,6 +5,7 @@ import data.exceptions.RequestExceptions;
 import data.interfaces.MapperInterface;
 import data.models.Request;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -21,7 +22,7 @@ public class RequestMapper implements MapperInterface<Request> {
     public List<Request> getAll() throws RequestExceptions {
         try (Connection con = new DBConnector().getConnection()) {
             List<Request> requests = new ArrayList();
-            String qry = "SELECT * FROM stock";
+            String qry = "SELECT * FROM requests";
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery(qry);
 
@@ -37,8 +38,8 @@ public class RequestMapper implements MapperInterface<Request> {
                         rs.getString("address"),
                         rs.getString("zipCode"),
                         rs.getString("phone"),
-                        rs.getString("email"))
-                        rs.getString("note"));
+                        rs.getString("email"),
+                        rs.getString("note")));
             }
             return requests;
         } catch (SQLException ex) {
@@ -49,10 +50,46 @@ public class RequestMapper implements MapperInterface<Request> {
 
     @Override
     public Request getById(int id) throws RequestExceptions {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try (Connection con = new DBConnector().getConnection()) {
+            
+            String qry = "SELECT * FROM requests WHERE id = ?";
+
+            PreparedStatement ps = con.prepareStatement(qry);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                return new Request(
+                        rs.getInt("width"),
+                        rs.getInt("length"),
+                        rs.getInt("shedWidth"),
+                        rs.getInt("shedLength"),
+                        rs.getString("roof"),
+                        rs.getInt("angle"),
+                        rs.getString("name"),
+                        rs.getString("address"),
+                        rs.getString("zipCode"),
+                        rs.getString("phone"),
+                        rs.getString("email"),
+                        rs.getString("note"));
+            }
+            return null;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RequestExceptions("Error occoured while getting data from database");
+        }
     }
     
     public void add(Request request) throws RequestExceptions {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public static void main(String[] args) throws RequestExceptions {
+        RequestMapper rm = new RequestMapper();
+        //rm.getAll();
+        List<Request> requests = new ArrayList();
+        for (Request r : requests) {
+            rm.getAll();
+        }
     }
 }
