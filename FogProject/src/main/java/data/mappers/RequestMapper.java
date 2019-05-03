2202bf5e14ffd2522c1ddf2acc44f9c879bd8fef
WSHuusfeldt <williamhuusfeldt@gmail.com>
2019-05-03 10:20:58 +0200

    Added more measurements to 2nd svg drawing, added getAll() for RequestMapper

--- FogProject/src/main/java/data/mappers/RequestMapper.java
+++ FogProject/src/main/java/data/mappers/RequestMapper.java
@@ -1,19 +1,50 @@
 package data.mappers;
 
+import data.DBConnector;
 import data.exceptions.RequestExceptions;
 import data.interfaces.MapperInterface;
 import data.models.Request;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  *
- * @author Asger Hermind Sørensen & Martin Frederiksen
+ * @author William Sehested Huusfeldt
  */
 public class RequestMapper implements MapperInterface<Request> {
 
     @Override
     public List<Request> getAll() throws RequestExceptions {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try (Connection con = new DBConnector().getConnection()) {
+            List<Request> requests = new ArrayList();
+            String qry = "SELECT * FROM stock";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(qry);
+
+            while (rs.next()) {
+                requests.add(new Request(
+                        rs.getInt("width"),
+                        rs.getInt("length"),
+                        rs.getInt("shedWidth"),
+                        rs.getInt("shedLength"),
+                        rs.getString("roof"),
+                        rs.getInt("angle"),
+                        rs.getString("name"),
+                        rs.getString("address"),
+                        rs.getString("zipCode"),
+                        rs.getString("phone"),
+                        rs.getString("email"))
+                        rs.getString("note"));
+            }
+            return requests;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RequestExceptions("Error occoured while getting data from database");
+        }
     }
 
     @Override
